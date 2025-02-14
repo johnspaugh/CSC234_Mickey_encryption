89ab8f2f7506b0c6132af7054ec8178897286c8e
John Spaugh <johnspaugh33@gmail.com>
2025-02-13 16:04:03 -0800

    commit so the main code up on the branch

--- include/AddPieToEnd.h
+++ include/AddPieToEnd.h
@@ -1,4 +1,36 @@
+/**
+ * CPE/CSC 159 - Operating System Pragmatics
+ * California State University, Sacramento
+ *
+ * Kernel Functions and Helpers
+ */
+#ifndef ADDPIETOEND_H
+#define ADDPIETOEND_H
 
+#ifndef OS_NAME
+#define OS_NAME "MyOS"
+#endif
+/**
+ * append the number Pie to password selected in the input
+ */
 string AddPieToEnd(string password);//
 
+/**
+ * truncate number Pie to password selected in the input
+ */
+string truncateString(const std::string& inputString)
+
+/**
+ * append the number Pie to password selected for comparison
+ */
+string retrievePassword(string password, int passwordNumber);//
+
+
+
+/**
+ * read content from file
+ */
 string readFile(string path);//
+
+
+#endif
--- src/AddPieToEnd.cpp
+++ src/AddPieToEnd.cpp
@@ -1,17 +1,37 @@
 
-// #include <storage/PieNumbers.txt>;
-// #include <password1>;
-// #include "password2";
-// #include <password3>;
-// #include "include/my_header.h"; 
-// #include "iostream";
+#include <stdlib> //<stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 
 namespace fs = std::filesystem;
 
-string AddPieToEnd(string password, int passwordNumber){
+
+
+string AddPieToEnd(string password){
+
+    string passwordPadding = NULL;
+    string pieString = readFile("/storage/PieNumbers.txt");
+
+    passwordPadding = password + pieString;
+    string result = truncateString(passwordPadding);
+
+    return result;
+}
+
+std::string truncateString(const std::string& inputString) {
+    //1KB = (1024 bytes)
+    const size_t maxSize = 1024; 
+    if (inputString.length() <= maxSize) {
+        //in theory should never have <= input, so message error
+        printf("the input is <= 1KB, should not happen when padding with pie.");
+        return inputString;
+    } else {
+        return inputString.substr(0, maxSize);
+    }
+}
+
+string retrievePassword(string password, int passwordNumber){
 
     string ModifiedPassword = NULL;
     string pieString = NULL;
@@ -20,10 +40,13 @@ string AddPieToEnd(string password, int passwordNumber){
     string filenamePassword1 = "password1.txt";
     string path = NULL;
     if(passwordNumber == 1){
+        // string of password
         path = "/storage/password1.txt";
     }else(passwordNumber == 2){
+        //this was picture
         path = "/storage/password2";
     }else(passwordNumber == 3){
+        // string of password
         path = "/storage/password3.txt";
     }else{
         printf("did not input the correct password path, so will exit system");
