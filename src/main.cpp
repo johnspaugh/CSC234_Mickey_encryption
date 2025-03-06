3e0e4fed91b0e2f880beb3cb588fc50def9c87b7
John Spaugh <johnspaugh33@gmail.com>
2025-03-06 15:17:22 -0800

    orgainzed into folders

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,25 @@
 using namespace std;
 
 int main() {
-    Node* root;
-    std::string code;
-    // std::string>& codes;
-    // std::map<unsigned char;
-    // generateCodes( root, code, std::map<unsigned char,  codes);
+    //Call on Huffman code
+    //  generateCodes
+    //  buildHuffmanTree
+    //  huffmanEncode
+    //  huffmanDecode
+
+    //Call on file_encryptor code
+    //  createIndices
+    //  getKey
+    //  parseOptions()  //for the cmd line
+    //  printMatrix()   //function is primarily for debugging 
+    //  readFile()
+    //  shiftX() shiftY() shiftZ() void
+    //  encode()  
+    //  decode()
+    //  writeFile()  //writes the encripted file to disk
+
+    //Output code 
+    
 
     return 0;
 }
